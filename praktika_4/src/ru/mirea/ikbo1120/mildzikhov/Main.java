c68c5a2950c1e876d3cd102d80e3274e99b4768d
PRO100CHEL <90497142+PRO100CHEL@users.noreply.github.com>
2021-11-06 03:43:54 +0300

    Add files via upload

---
+++ praktika_4/src/ru/mirea/ikbo1120/mildzikhov/Main.java
@@ -0,0 +1,8 @@
+package ru.mirea.ikbo1120.mildzikhov;
+
+import ru.mirea.ikbo1120.mildzikhov.classes.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Main { public static void main(String[] args) {new FootballScore();}}
