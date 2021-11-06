e68b1386e5397a0676fa33e24e8d676eff2b8ae5
PRO100CHEL <90497142+PRO100CHEL@users.noreply.github.com>
2021-11-06 03:43:41 +0300

    Delete praktika_4 directory

--- praktika_4/src/ru/mirea/ikbo1120/mildzikhov/Main.java
+++
@@ -1,8 +0,0 @@
-package ru.mirea.ikbo1120.mildzikhov;
-
-import ru.mirea.ikbo1120.mildzikhov.classes.*;
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-
-public class Main { public static void main(String[] args) {new FootballScore();}}
